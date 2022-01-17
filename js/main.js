2bd86d6038d4d9dce4ff54a0b6bf45d4505f9f9a
Klaudia <klau.banach@gmail.com>
2022-01-17 19:35:00 +0100

    added working mobile nav

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 const menuBtn = document.querySelector(".menu-btn")
-const navItems = document.getElementById(".nav-items")
+const navItems = document.getElementById("nav-items")
 const closeBtn = document.querySelector(".close-btn")
 
 
 
 const showMobileNav = () => {
-    navItems.classList.add('active')
+    navItems.classList.add('mobile')
     
 }
 
 const closeMobileNav = () => {
-    navItems.classList.remove('active')
+    navItems.classList.remove('mobile')
 }
 
 menuBtn.addEventListener('click', showMobileNav)
