4d837ffb32bb2abd4744ed0da85cffae6a5eae0b
Klaudia <klau.banach@gmail.com>
2022-01-17 19:53:49 +0100

    added js close to items mobile nav

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 const menuBtn = document.querySelector(".menu-btn")
 const navItems = document.getElementById("nav-items")
+const navItem = document.querySelectorAll(".nav-item")
 const closeBtn = document.querySelector(".close-btn")
 
 
 
 const showMobileNav = () => {
-    navItems.classList.add('mobile')
+    navItems.classList.add('mobile');
+
+    navItem.forEach(item => {
+        item.addEventListener('click', () => {
+            navItems.classList.remove('mobile')
+        })
+    })
     
 }
 
 const closeMobileNav = () => {
     navItems.classList.remove('mobile')
 }
+
 
 menuBtn.addEventListener('click', showMobileNav)
 closeBtn.addEventListener('click', closeMobileNav)
